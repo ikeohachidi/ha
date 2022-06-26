c42d31f902eef3f35ca97c6792c29210271ac417
ikeohachidi <ikeohachidi@gmail.com>
2022-06-26 11:13:03 -0700

    fix: responsive of homepage

--- src/App.style.ts
+++ src/App.style.ts
@@ -4,11 +4,11 @@ export const AppShell = styled.div`
     padding: 40px;
 `
 
-export const CardsWrapper = styled.div`
+export const CardsWrapper = styled.ul`
     max-width: 1920px;
     list-style: none;
     padding-left: 0;
     display: grid;
-    grid-template-columns: repeat(4, 1fr);
+    grid-template-columns: repeat(auto-fit, minmax(300px,  1fr));
     gap: 30px;
 `
